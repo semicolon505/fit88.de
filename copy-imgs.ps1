d208f66a0f8111c17cf444a2af3626dea3ff99ed
Raneem <git config --global user.name>
2026-08-29 02:07:12 +0300

    Remove unused files

--- copy-imgs.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$src = "C:\Users\Raneem\Desktop\fit88\assets"
-$dst = "C:\Users\Raneem\Desktop\fit88.de\client\src\img"
-Copy-Item "$src\hero-doctor-lineart.png" "$dst\hero-lineart.png" -Force
-Copy-Item "$src\logo-transparent.png"   "$dst\logo.png" -Force
-Get-ChildItem $dst | Select-Object Name, Length | Format-Table -AutoSize
-"COPIED_OK"
--- img-size.ps1
+++
@@ -1,8 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$files = @("C:\Users\Raneem\Desktop\fit88.de\client\src\img\logo.png",
-           "C:\Users\Raneem\Desktop\fit88.de\client\src\img\hero-lineart.png")
-foreach ($f in $files) {
-  $img = [System.Drawing.Image]::FromFile($f)
-  "${f}: $($img.Width) x $($img.Height)"
-  $img.Dispose()
-}
